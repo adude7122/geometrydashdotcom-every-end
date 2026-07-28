87abf29e8768fc430009304753a4394e2ed2a9a1
adude7122 <jacknap234@gmail.com>
2026-07-28 14:48:33 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -18,6 +18,10 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);  
   
